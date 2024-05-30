c9a38e124665e43ac3d712d67121480adc9ad874
thederpykrafter <thederpykrafter@gmail.com>
2024-05-30 02:55:35 -0400

    switch to ultima theme

--- user.js
+++ user.js
@@ -1,17 +1,24 @@
-//
+user_pref("ultima.tabs.vertical", false);
+user_pref("ultima.tabs.xs", false);
+user_pref("ultima.tabs.s", false);
+user_pref("ultima.tabs.l", true);
+user_pref("ultima.tabs.autohide", true);
+
+user_pref("ultima.sidebar.autohide", true);
+
+user_pref("ultima.xstyle.squared", false);
+user_pref("ultima.xstyle.containertabs.i", false);
+user_pref("ultima.xstyle.containertabs.ii", false);
+user_pref("ultima.xstyle.containertabs.iii", true);
+user_pref("ultima.xstyle.closetabsbutton", false);
+
+user_pref("ultima.theme.extensions", true);
+
+user_pref("ultima.OS.kde", false);
+user_pref("ultima.OS.gnome", true);
+user_pref("ultima.OS.mac", false);
+user_pref("ultima.OS.linux", false);
 
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-
-user_pref("browser.sessionstore.restore_pinned_tabs_on_demand", true);
-
-user_pref("browser.compactmode.show", true);
-
-user_pref("browser.toolbars.bookmarks.visibility", "never");
-
-user_pref("browser.uidensity", 1);
-
-user_pref("browser.download.autohideButton", true);
-
-user_pref("ui.key.menuAccessKeyFocuses", false);
-
-user_pref("ui.key.menuAccessKey", 0);
+user_pref("svg.context-properties.content.enabled", true);
+user_pref("toolkit.tabbox.switchByScrolling", true);
